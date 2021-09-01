1afcb59274b6aa265d88c48de5645784d558a205
John Cleveland <john@zippymeals.com>
2021-09-01 01:32:55 -0400

    Added an initial set of files.

---
+++ queries.sql
@@ -0,0 +1,22 @@
+select
+    ac.year as Year,
+    ac.name as Category,
+    w.name as Name,
+    a.first_name || ' ' || a.last_name as Author
+from
+    work w
+inner join
+    work_author wa on wa.work_id = w.id
+inner join
+    author a on a.id = wa.author_id
+inner join
+    work_award_category wac on wac.work_id = w.id
+inner join
+    award_category ac on ac.id = wac.award_category_id = ac.id
+inner join
+    award aw on aw.id = ac.award_id
+where
+    aw.name = 'Hugo Award'
+order by
+    ac.year,
+    wac.status;
---
+++ tables.sql
@@ -0,0 +1,45 @@
+DROP TABLE IF EXISTS author;
+DROP TABLE IF EXISTS work;
+DROP TABLE IF EXISTS award;
+DROP TABLE IF EXISTS award_category;
+DROP TABLE IF EXISTS work_author;
+DROP TABLE IF EXISTS work_award_category;
+
+CREATE TABLE author (
+    id INTEGER PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    middle_name TEXT,
+    last_name TEXT NOT NULL
+);
+
+CREATE TABLE work (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    type TEXT NOT NULL
+);
+
+CREATE TABLE award (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE award_category (
+    id INTEGER PRIMARY KEY,
+    award_id INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    sort_order INTEGER NOT NULL,
+    year INTEGER NOT NULL
+);
+
+CREATE TABLE work_author (
+    id INTEGER PRIMARY KEY,
+    work_id INTEGER NOT NULL,
+    author_id INTEGER NOT NULL
+);
+
+CREATE TABLE work_award_category (
+    id INTEGER PRIMARY KEY,
+    work_id INTEGER NOT NULL,
+    award_category_id INTEGER NOT NULL,
+    status INTEGER NOT NULL
+);
