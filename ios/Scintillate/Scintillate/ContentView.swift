5d1c482353f1229112ac3a997d88906f202a4273
John Cleveland <john@zippymeals.com>
2021-12-11 16:01:21 -0500

    Designing the cards.

--- ios/Scintillate/Scintillate/ContentView.swift
+++ ios/Scintillate/Scintillate/ContentView.swift
@@ -1,9 +1,51 @@
 import SwiftUI
 
 extension Color {
-    static let app1 = Color(red: 28/255, green: 55/255, blue: 85/255)
-    static let app2 = Color(red: 23/255, green: 40/255, blue: 85/255)
-    static let app3 = Color(red: 115/255, green: 234/255, blue: 216/255)
+    static let app1 = Color(red: 50/255, green: 50/255, blue: 50/255)
+    static let app2 = Color(red: 30/255, green: 30/255, blue: 30/255)
+    static let app3 = Color.black
+}
+
+struct BookImageView: View {
+    var uiImage: UIImage
+    var checkmarkImageName: String
+
+    var body: some View {
+        ZStack {
+            Image(uiImage: uiImage)
+                 .resizable()
+                 .aspectRatio(contentMode: .fit)
+                 .padding(7)
+//                 .border(Color.white, width: 0.25)
+            VStack {
+                HStack {
+                    Spacer()
+                    Image(checkmarkImageName)
+                         .resizable()
+                         .aspectRatio(contentMode: .fit)
+                         .frame(width: 20)
+                         .padding(7)
+                }
+                Spacer()
+            }
+        }
+    }
+}
+
+struct BookInfoView: View {
+    var work: Work
+
+    var body: some View {
+        Text(work.title)
+            .font(.custom("Arial", size: 9))
+            .lineLimit(1)
+            .truncationMode(.tail)
+            .allowsTightening(true)
+        Text(work.getAuthorText())
+            .font(.custom("Arial", size: 9))
+        Text(work.getAwardText())
+            .font(.custom("Arial", size: 9))
+    }
 }
 
 struct ContentView: View {
@@ -11,49 +53,29 @@ struct ContentView: View {
 
     var body: some View {
         let size: CGFloat = 115
-        let padding: CGFloat = 20
-        
+        let padding: CGFloat = 11
+
         ScrollView {
             LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())],
                       spacing: padding) {
                 ForEach(store.db.work.getHugoWinnersOnly(), id: \.self) { work in
-                    let uiImage =  (UIImage(named: work.imageName) ?? UIImage(named: "default-cover"))!
-                    let borderColor = work.id % 2 == 0 ? Color.white : Color.white
-                    let borderWidth = 0.25 //work.id % 2 == 0 ? 0.25 : 0.25
-                    let checkmarkImageName = work.id % 3 == 0 ? "checkmark" : "checkmark-placeholder"
-                    
+                    let uiImage: UIImage =  (UIImage(named: work.imageName) ?? UIImage(named: "default-cover"))!
+                    let checkmarkImageName: String = work.id % 3 == 0 ? "checkmark" : "checkmark-placeholder"
+
                     VStack {
-                        ZStack {
-                            Image(uiImage: uiImage)
-                                 .resizable()
-                                 .aspectRatio(contentMode: .fit)
-                                 .border(borderColor, width: borderWidth)
-                            VStack {
-                                HStack {
-                                    Spacer()
-                                    Image(checkmarkImageName)
-                                         .resizable()
-                                         .aspectRatio(contentMode: .fit)
-                                         .frame(width: 20)
-                                         .padding(2)
-                                }
-                                Spacer()
-                            }
-                        }
-                        Text(work.title)
-                            .font(.custom("Arial", size: 9))
-                            .lineLimit(1)
-                            .truncationMode(.tail)
-                            .allowsTightening(true)
-                        Text(work.getAuthorText())
-                            .font(.custom("Arial", size: 9))
-                        Text(work.getAwardText())
-                            .font(.custom("Arial", size: 9))
+                        BookImageView(uiImage: uiImage, checkmarkImageName: checkmarkImageName)
+                        BookInfoView(work: work)
                     }
-                    .padding(.horizontal, 10)
+//                    .border(Color.white, width: 0.25)
+                    .padding(10)
+
+                    .background(Color.app1.ignoresSafeArea())
+                    .padding(10)
+
                 }
             }
         }
+        .background(Color.app2.ignoresSafeArea())
 
     }
 }
